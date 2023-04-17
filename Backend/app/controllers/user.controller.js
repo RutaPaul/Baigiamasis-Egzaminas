8bc2a18f50a625d2f2c8134d961877161b6c207f
RutaPaul <rutapaulaviciute14@gmail.com>
2023-04-17 19:42:14 +0300

    Implemented register/login routes, with password hashing

--- Backend/app/controllers/user.controller.js
+++ Backend/app/controllers/user.controller.js
@@ -1,23 +1,39 @@
 const User = require("../models/user.models.js");
+const bcrypt = require("bcrypt");
 
 exports.controlUser = (req,res) => {
 
     switch(req.params.type.toUpperCase()){
         case "REGISTER":
-            const newUser = new User({
-                Username: req.body.Username,
-                Password: req.body.Password
-            });
-            User.Register(newUser, (err,data)=>{
-                if (err)
-                res.status(500).send({message: err.message || "Some error occurred while creating new user."});
-                else res.send(data);
-            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                if(err) console.log(err);
+                bcrypt.hash(req.body.Password, salt, function(err, hash) {
+                    if(err) console.log(err);
+
+                    const newUser = new User({
+                        Username: req.body.Username,
+                        Password: hash
+                    });
+
+                    User.Register(newUser, (err,data)=>{
+                        if (err)
+                        res.status(500).send({message: err.message || "Some error occurred while creating new user."});
+                        else res.send(data);
+                    })
+                });
+            })  
+            
         break;
 
         case "LOGIN":
-            User.Login(req, (err,data)=>{
-                
+            User.Login(req.body.Username, req.body.Password, (err,data, success)=>{
+                if (err)
+                    res.status(500).send({message: err.message || "Some error occurred while creating new user."});
+                else if(success)
+                    res.status(200).send({username:data})
+                else 
+                    res.status(404).send({message: data})
             })
         break;
 
--- Backend/app/models/user.models.js
+++ Backend/app/models/user.models.js
@@ -1,4 +1,5 @@
 const sql = require("./db.js");
+const bcrypt = require("bcrypt");
 
 const User = function(user){
     this.Username = user.Username;
@@ -6,16 +7,61 @@ const User = function(user){
 }
 
 User.Register = (newUser, result) => {
-    sql.query("INSERT INTO USERS SET ?", newUser, (err, res) => {
+
+    let query = `SELECT * FROM USERS `;
+    query += `WHERE USERS.Username = '${newUser.Username}' `
+
+
+    sql.query(query, (err,res)=>{ 
         if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
+            console.log("error: ", err);
+            result(err, null);
+            return;
         }
-    
-        console.log("created new user: ", { id: res.insertId, ...newUser });
-        result(null, { id: res.insertId, ...newUser });
-      });
+
+        if(res.length == 0){
+            sql.query("INSERT INTO USERS SET ?", newUser, (err, res) => {
+                if (err) {
+                  console.log("error: ", err);
+                  result(err, null);
+                  return;
+                }
+            
+                console.log("created new user: ", { id: res.insertId, ...newUser });
+                result(null, { id: res.insertId, ...newUser });
+              });
+
+        }
+        else {
+            result({message:"Username is taken"}, null);
+        }
+    })
+}
+
+User.Login = (username, password, result) =>{
+    let query = `SELECT * FROM USERS `;
+    query += `WHERE USERS.Username = '${username}' `
+    sql.query(query, (err,res)=>{
+        if(err){
+            console.log("error: ", err);
+            result(err, null, false);
+            return;
+        }
+
+        if(res.length == 0){
+            result(null, "Incorrect Username", false);
+        }
+
+        if(res.length == 1){
+            bcrypt.compare(password, res[0].Password, function(err, auth) {
+                if(err) result(err,null,false);
+                if(auth) result(null, username, true)
+                else result(null, "Incorrect Password", false)
+            });
+
+        }
+    })
+
 }
 
 
